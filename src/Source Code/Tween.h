f8913fa2f11801748a0c8bf1147155dc35d9c574
MayKoder <sg.miquel@gmail.com>
2022-01-27 21:52:11 +0100

    Skybox added and tween fixed

--- src/Source Code/Tween.h
+++ src/Source Code/Tween.h
@@ -5,41 +5,49 @@
 template<class T>
 struct Tween
 {
-	void Set(T min, T max, float _speed, bool playOnStart = false)
+
+	~Tween()
 	{
-		minValue = min;
-		maxValue = max;
+		delete ptrValue;
+	}
+	void Set(T min, T max, float _duration, bool playOnStart = false)
+	{
+		a = min;
+		b = max;
 
-		(playOnStart == false) ? value = min : value = max;
+		//(playOnStart == false) ? value = min : value = max;
+		ptrValue = new T();
 
-		speed = _speed;
+		*ptrValue = a;
+
+		tweenDuration = _duration;
 		t = 0.f;
 	}
-	void Set(T min, T max, float _speed, T* _pointer, bool playOnStart = false)
-	{
-		minValue = min;
-		maxValue = max;
+	//void Set(T min, T max, float _speed, T* _pointer, bool playOnStart = false)
+	//{
+	//	minValue = min;
+	//	maxValue = max;
 
-		(playOnStart == false) ? value = min : value = max;
-		ptrValue = _pointer;
+	//	(playOnStart == false) ? value = min : value = max;
+	//	ptrValue = _pointer;
 
-		speed = _speed;
-		t = 0.f;
-	}
+	//	speed = _speed;
+	//	t = 0.f;
+	//}
 
 	T Get() {
-		return value;
+		return *ptrValue;
 	}
-	T* GetPTR() {
-		return ptrValue;
-	}
+	//T* GetPTR() {
+	//	return ptrValue;
+	//}
 
 	void Invert() {
 		isActive = true;
-		T temp = minValue;
+		T temp = a;
 
-		minValue = maxValue;
-		maxValue = temp;
+		a = b;
+		b = temp;
 
 		t = 0.f;
 		//value = minValue;
@@ -49,15 +57,16 @@ struct Tween
 	{
 		if (isActive == true)
 		{
-			value = Step(dt);
+			*ptrValue = Step(dt);
 		}
-		return value;
+		return *ptrValue;
 	}
-	void StepByPointer(float dt)
-	{
-		assert(ptrValue == nullptr, "PTR is nullptr, something is wrong")
-			* ptrValue = Step(dt)
-	}
+
+	//void StepByPointer(float dt)
+	//{
+	//	assert(ptrValue == nullptr, "PTR is nullptr, something is wrong")
+	//		* ptrValue = Step(dt)
+	//}
 
 	T Step(float dt)
 	{
@@ -65,51 +74,67 @@ struct Tween
 	}
 
 private:
-	T maxValue;
-	T minValue;
+	T a;
+	T b;
 
-
-
-	T value;
 	T* ptrValue = nullptr; //Maybe we could use only the pointer? not sure
 
 public:
-	float speed;
+
+	float tweenDuration;
 	float t;
 	bool isActive = false;
+
 	std::function<T(Tween<T>*, float)> stepOverride = [](Tween<T>* ent, float dt) 
 	{
-		ent->t += dt * ent->speed;
+		ent->t += dt;
 
-		//t = (t < 0.5 ? pow(2, 20 * t - 10) / 2
-		//	: (2 - pow(2, -20 * t + 10)) / 2) * 40;
-
-		if (ent->t >= 1.0) {
-			ent->t = 0.0;
+		if (ent->t >= ent->tweenDuration) {
+			ent->t = 0.0f;
 			ent->isActive = false;
+			return ent->b;
 		}
 
-		//T tmp = -2 * value + 2;
-		//value = t < 0.5 ? 8 * value * value * value * value : 1 - (tmp*tmp*tmp*tmp) / 2;
+		return ent->a + (ent->b - ent->a) * (ent->t / ent->tweenDuration);
+		//return (ent->value * (1.0 - ent->t)) + (ent->maxValue * ent->t);
+		//return (ent->minValue * (1.0f - ent->t)) + (ent->maxValue * ent->t);
 
-		return (ent->value * (1.0 - ent->t)) + (ent->maxValue * ent->t);
-		//return (t * maxValue) + (1.0 - t) * value;
+		//return ent->minValue + ent->t * (ent->maxValue - ent->minValue);
 	};
+
+	//std::function<T(Tween<T>*, float)> stepOverride = [](Tween<T>* ent, float dt) 
+	//{
+	//	ent->t += dt * ent->speed;
+
+	//	//t = (t < 0.5 ? pow(2, 20 * t - 10) / 2
+	//	//	: (2 - pow(2, -20 * t + 10)) / 2) * 40;
+
+	//	if (ent->t >= 1.0) {
+	//		ent->t = 0.0;
+	//		ent->isActive = false;
+	//	}
+
+	//	//T tmp = -2 * value + 2;
+	//	//value = t < 0.5 ? 8 * value * value * value * value : 1 - (tmp*tmp*tmp*tmp) / 2;
+
+	//	return (ent->value * (1.0 - ent->t)) + (ent->maxValue * ent->t);
+	//	//return (t * maxValue) + (1.0 - t) * value;
+	//};
 };
 
-#include"Globals.h"
-namespace CustomEasings
-{
-	static std::function<float(Tween<float>*, float)> easeInOutBounce = [](Tween<float>* ent, float dt)
-	{
-		ent->t += dt * ent->speed;
-		float t = ent->t;
-
-		if (t < 0.5) {
-			return 8 * pow(2, 8 * (t - 1)) * abs(sin(t * PI * 7));
-		}
-		else {
-			return 1 - 8 * pow(2, -8 * t) * abs(sin(t * PI * 7));
-		}
-	};
-}
+//#include"Globals.h"
+//namespace CustomEasings
+//{
+//	static std::function<float(Tween<float>*, float)> easeInOutBounce = [](Tween<float>* ent, float dt)
+//	{
+//		ent->t += dt;
+//		float t = ent->t;
+//
+//		if (t < 0.5) {
+//			return 8 * pow(2, 8 * (t - 1)) * abs(sin(t * PI * 7));
+//		}
+//		else {
+//			return 1 - 8 * pow(2, -8 * t) * abs(sin(t * PI * 7));
+//		}
+//	};
+//}
